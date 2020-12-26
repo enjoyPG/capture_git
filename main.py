31505903199812075d855542e8e3ca443d81b841
EnjoyPG <lasthere7@gmail.com>
2020-12-26 15:44:12 +0900

    grayscale

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import keyboard
 
 def takeshot():
     print('takeshot')
-    for i in range(120):
+    for i in range(2):
         now = time.localtime()
         ntime = "%04d-%02d-%02d-%02dh-%02dm-%02ds--%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour,now.tm_min, now.tm_sec, i)
         print(ntime)
+        #color
         img = ImageGrab.grab((0, 0, 800, 550))
         fname = "{}{}".format(ntime, '.png')
         img.save('./img/lie/' + fname)
+        #grayscale
+        imgG = img.convert('L')
+        fname = "{}{}".format(ntime, '.png')
+        imgG.save('./img/lieG/' + fname)
         time.sleep(0.5)
 # img.show()
 
